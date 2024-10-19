28f804f5d60aa0719e4d7f1fe1ed842acdbde5d2
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-19 23:03:15 +0530

    added implemetation of methods to get and set cache

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"context"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -13,8 +14,17 @@ type WeatherCache interface {
 
 type cache struct {
 	rdb *redis.Client
+	ctx context.Context
 }
 
-func NewCache(rdb *redis.Client) WeatherCache {
-	return &cache{rdb}
+func NewCache(rdb *redis.Client, ctx context.Context) WeatherCache {
+	return &cache{rdb, ctx}
+}
+
+func (c *cache) GetCache(key string) (string, error) {
+	return c.rdb.Get(c.ctx, key).Result()
+}
+
+func (c *cache) SetCache(key string, value string, expiration time.Duration) error {
+	return c.rdb.Set(c.ctx, key, value, expiration).Err()
 }
