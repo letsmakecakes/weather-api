5b90b0860d7eee51983ea3baa4c3dc1c9abd4712
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-20 15:37:45 +0530

    added method to respond with error

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,6 +2,10 @@ package utils
 
 import "github.com/gin-gonic/gin"
 
+func RespondWithError(ctx *gin.Context, code int, message string) {
+	RespondWithJSON(ctx, code, gin.H{"error": message})
+}
+
 func RespondWithJSON(ctx *gin.Context, code int, payload interface{}) {
 	ctx.JSON(code, payload)
 }
