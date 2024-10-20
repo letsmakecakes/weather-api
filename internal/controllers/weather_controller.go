e4ba2f2f8389e05841bb8272381c069d3b288935
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-20 15:48:29 +0530

    added method get weather

--- internal/controllers/weather_controller.go
+++ internal/controllers/weather_controller.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "weatherapi/internal/services"
+import (
+	"net/http"
+	"weatherapi/internal/services"
+	"weatherapi/pkg/utils"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
 
 type WeatherController struct {
 	Service services.WeatherService
@@ -8,4 +15,18 @@ type WeatherController struct {
 
 func NewWeatherController(service services.WeatherService) *WeatherController {
 	return &WeatherController{service}
+}
+
+func (c *WeatherController) GetWeather(ctx *gin.Context) {
+	city := ctx.Param("city")
+
+	weather, err := c.Service.GetWeather(city)
+	if err != nil {
+		log.Errorf("error getting blog: %v", err)
+		utils.RespondWithError(ctx, http.StatusInternalServerError, "Failed to retreive weather")
+	} else if weather == nil {
+		utils.RespondWithError(ctx, http.StatusNotFound, "Weather not found")
+	}
+
+	utils.RespondWithJSON(ctx, http.StatusOK, weather)
 }
