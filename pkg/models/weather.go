5730ecf0c472d0cd5b3fcb3b8781cbdec8802989
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-20 23:01:07 +0530

    added changes in struct model

--- pkg/models/weather.go
+++ pkg/models/weather.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 type Weather struct {
 	QueryCost       int     `json:"queryCost"`
 	Latitude        float64 `json:"latitude"`
@@ -14,40 +12,40 @@ type Weather struct {
 }
 
 type Day struct {
-	DateTime       time.Time `json:"datetime"`
-	DateTimeEpoch  int64     `json:"datetimeEpoch"`
-	TempMax        float64   `json:"tempmax"`
-	TempMin        float64   `json:"tempmin"`
-	Temp           float64   `json:"temp"`
-	FeelsLikeMax   float64   `json:"feelslikemax"`
-	FeelsLikeMin   float64   `json:"feelslikemin"`
-	FeelsLike      float64   `json:"feelslike"`
-	Dew            float64   `json:"dew"`
-	Humidity       float64   `json:"humidity"`
-	Precip         float64   `json:"precip"`
-	PrecipProb     float64   `json:"precipprob"`
-	PrecipCover    float64   `json:"precipcover"`
-	PrecipType     []string  `json:"preciptype"`
-	Snow           float64   `json:"snow"`
-	SnowDepth      float64   `json:"snowdepth"`
-	WindGust       float64   `json:"windgust"`
-	WindSpeed      float64   `json:"windspeed"`
-	WindDir        float64   `json:"winddir"`
-	Pressure       float64   `json:"pressure"`
-	CloudCover     float64   `json:"cloudcover"`
-	Visibility     float64   `json:"visibility"`
-	SolarRadiation float64   `json:"solarradiation"`
-	SolarEnergy    float64   `json:"solarenergy"`
-	UVIndex        int       `json:"uvindex"`
-	SevereRisk     int       `json:"severerisk"`
-	Sunrise        string    `json:"sunrise"`
-	SunriseEpoch   int64     `json:"sunriseEpoch"`
-	Sunset         string    `json:"sunset"`
-	SunsetEpoch    int64     `json:"sunsetEpoch"`
-	MoonPhase      float64   `json:"moonphase"`
-	Conditions     float64   `json:"conditions"`
-	Description    string    `json:"description"`
-	Icon           string    `json:"icon"`
-	Stations       []string  `json:"stations"`
-	Source         string    `json:"source"`
+	DateTime       string   `json:"datetime"`
+	DateTimeEpoch  int64    `json:"datetimeEpoch"`
+	TempMax        float64  `json:"tempmax"`
+	TempMin        float64  `json:"tempmin"`
+	Temp           float64  `json:"temp"`
+	FeelsLikeMax   float64  `json:"feelslikemax"`
+	FeelsLikeMin   float64  `json:"feelslikemin"`
+	FeelsLike      float64  `json:"feelslike"`
+	Dew            float64  `json:"dew"`
+	Humidity       float64  `json:"humidity"`
+	Precip         float64  `json:"precip"`
+	PrecipProb     float64  `json:"precipprob"`
+	PrecipCover    float64  `json:"precipcover"`
+	PrecipType     []string `json:"preciptype"`
+	Snow           float64  `json:"snow"`
+	SnowDepth      float64  `json:"snowdepth"`
+	WindGust       float64  `json:"windgust"`
+	WindSpeed      float64  `json:"windspeed"`
+	WindDir        float64  `json:"winddir"`
+	Pressure       float64  `json:"pressure"`
+	CloudCover     float64  `json:"cloudcover"`
+	Visibility     float64  `json:"visibility"`
+	SolarRadiation float64  `json:"solarradiation"`
+	SolarEnergy    float64  `json:"solarenergy"`
+	UVIndex        float64  `json:"uvindex"`
+	SevereRisk     float64  `json:"severerisk"`
+	Sunrise        string   `json:"sunrise"`
+	SunriseEpoch   int64    `json:"sunriseEpoch"`
+	Sunset         string   `json:"sunset"`
+	SunsetEpoch    int64    `json:"sunsetEpoch"`
+	MoonPhase      float64  `json:"moonphase"`
+	Conditions     string   `json:"conditions"`
+	Description    string   `json:"description"`
+	Icon           string   `json:"icon"`
+	Stations       []string `json:"stations"`
+	Source         string   `json:"source"`
 }
