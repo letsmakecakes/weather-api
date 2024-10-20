e1b18984abac62119c231363c947c0d5da3d9774
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-20 15:11:23 +0530

    implemented weather interface method

--- internal/services/weather_service.go
+++ internal/services/weather_service.go
@@ -1,6 +1,11 @@
 package services
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 	"weatherapi/internal/cache"
 	"weatherapi/pkg/models"
 )
@@ -10,5 +15,51 @@ type WeatherService interface {
 }
 
 type weatherService struct {
-	cache cache.WeatherCache
+	cache      cache.WeatherCache
+	apiKey     string
+	weatherURL string
+}
+
+func NewWeatherService(cache cache.WeatherCache, apiKey string, weatherURL string) WeatherService {
+	return &weatherService{cache, apiKey, weatherURL}
+}
+
+func (w *weatherService) GetWeather(city string) (*models.Weather, error) {
+	if city == "" {
+		return nil, fmt.Errorf("city is required")
+	}
+
+	// check if the weather data is in cache
+	val, err := w.cache.GetCache(city)
+	if err == nil && val != "" {
+		var cachedWeather models.Weather
+		err = json.Unmarshal([]byte(val), &cachedWeather)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshalling JSON: %v", err)
+		}
+
+		return &cachedWeather, nil
+	}
+
+	// if not in cache, fetch from 3rd party API
+	weatherURL := fmt.Sprintf(w.weatherURL+"?location=%s&key=%s", city, w.apiKey)
+
+	resp, err := http.Get(weatherURL)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to featch weather data")
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read weather data")
+	}
+
+	var weather models.Weather
+	err = json.Unmarshal(body, &weather)
+
+	// Cache the weather data with a 12-hour expiration
+	w.cache.SetCache(city, body, 12*time.Hour)
+
+	return &weather, nil
 }
