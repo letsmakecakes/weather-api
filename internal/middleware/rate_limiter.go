dfd7e0a426f41db3d9745f2f674a641dd5b02900
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-20 21:00:40 +0530

    added method to return a new rate limiter instance

--- internal/middleware/rate_limiter.go
+++ internal/middleware/rate_limiter.go
@@ -11,3 +11,10 @@ type RateLimiter struct {
 	clients map[string]*rate.Limiter
 	mu      sync.Mutex
 }
+
+// NewRateLimiter creates a new rate limiter instance
+func NewRateLimiter() *RateLimiter {
+	return &RateLimiter{
+		clients: make(map[string]*rate.Limiter),
+	}
+}
