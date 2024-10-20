aed068f9de046efecd56aa511cb03af01c7184ad
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-20 20:50:45 +0530

    added method to load config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,28 @@
 package config
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
-	Port string
-	DatabaseURL string
-	Environment string
+	Port                    string
+	DatabaseURL             string
+	WeatherThirdPartyAPIURL string
+	Environment             string
 }
 
+func LoadConfig() (*Config, error) {
+	// Load .env file
+	if err := godotenv.Load("../../.env"); err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		Port:                    os.Getenv("PORT"),
+		DatabaseURL:             os.Getenv("DATABASE_URL"),
+		WeatherThirdPartyAPIURL: os.Getenv("WEATHER_THIRDPARTY_API_URL"),
+		Environment:             os.Getenv("ENVIRONMENT"),
+	}, nil
+}
